43cf5739764ca8029945a237507cff942df748a8
Enja-Anderson <110401060+Enja-Anderson@users.noreply.github.com>
2022-08-05 08:22:17 -0600

    Added rout for MESSAGE_STYLE=uppercase

--- myApp.js
+++ myApp.js
@@ -18,9 +18,14 @@ let stylePath = __dirname + "/public";
 app.use("/public", express.static(stylePath));
 
 
-
+//need to configure dotenv for this to work locally
+//for freeCodeCamp challenge, a secret (hidden) variable was created in replit.com. key: MESSAGE_STYLE value: uppercase
 app.get("/json", (req, res) => {
-    res.json({"message": "Hello json"});
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        res.json({"message": "HELLO JSON"});
+    } else {
+        res.json({"message": "Hello json"});
+    }
   });
 
 
